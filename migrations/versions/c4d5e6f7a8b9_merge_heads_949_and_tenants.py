d8aaebc5f6616f954d23f5fbda81b8f9add0a84f
Ahmad_Gannam <rafideen.ahmadghannam@gmail.com>
2026-05-21 15:31:44 +0300

    Merge alembic heads for upgrade

---
+++ migrations/versions/c4d5e6f7a8b9_merge_heads_949_and_tenants.py
@@ -0,0 +1,22 @@
+"""merge heads 949 and tenants
+
+Revision ID: c4d5e6f7a8b9
+Revises: 94948c531c03, b2c3d4e5f6a7
+Create Date: 2026-05-21
+"""
+from alembic import op
+
+
+revision = "c4d5e6f7a8b9"
+down_revision = ("94948c531c03", "b2c3d4e5f6a7")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
+
